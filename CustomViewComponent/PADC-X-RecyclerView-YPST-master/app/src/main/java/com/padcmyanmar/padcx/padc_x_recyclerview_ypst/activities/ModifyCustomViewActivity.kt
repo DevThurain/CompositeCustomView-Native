5ba1bf959cb8c06021352f305fbe0e553261be99
thurain <thurainbowin.dev@gmail.com>
2022-10-19 11:30:01 +0630

    - state progress

--- CustomViewComponent/PADC-X-RecyclerView-YPST-master/app/src/main/java/com/padcmyanmar/padcx/padc_x_recyclerview_ypst/activities/ModifyCustomViewActivity.kt
+++ CustomViewComponent/PADC-X-RecyclerView-YPST-master/app/src/main/java/com/padcmyanmar/padcx/padc_x_recyclerview_ypst/activities/ModifyCustomViewActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import com.padcmyanmar.padcx.padc_x_recyclerview_ypst.R
 import com.padcmyanmar.padcx.padc_x_recyclerview_ypst.veiws.components.EmotionalFaceView
+import com.padcmyanmar.padcx.padc_x_recyclerview_ypst.veiws.viewpods.StateProgressViewPod
 import kotlinx.android.synthetic.main.activity_modify_custom_view.*
 
 class ModifyCustomViewActivity: BaseActivity() {
@@ -13,11 +14,21 @@ class ModifyCustomViewActivity: BaseActivity() {
             return Intent(context, ModifyCustomViewActivity::class.java)
         }
     }
+    private lateinit var mViewPodStateProgress: StateProgressViewPod
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_modify_custom_view)
 
+        setUpViewPod()
         setupListeners()
+    }
+
+    private fun setUpViewPod() {
+        mViewPodStateProgress = viewPodStateProgress as StateProgressViewPod
+        mViewPodStateProgress.apply {
+            setStepDescription("Survey", "Cabling", "Splicing", "Activate")
+        }
     }
 
     fun setupListeners(){
@@ -29,6 +40,24 @@ class ModifyCustomViewActivity: BaseActivity() {
         // 2
         btnSad.setOnClickListener {
             emotionalFaceView.happinessState = EmotionalFaceView.SAD
+        }
+
+
+        btn1.setOnClickListener {
+            mViewPodStateProgress.onTapStep1()
+        }
+
+        btn2.setOnClickListener {
+            mViewPodStateProgress.onTapStep2()
+        }
+
+        btn3.setOnClickListener {
+            mViewPodStateProgress.onTapStep3()
+        }
+
+        btn4.setOnClickListener {
+            mViewPodStateProgress.onTapStep4()
+            mViewPodStateProgress.completeAllSteps()
         }
     }
 }
